3609589931f9b7626d1b3a1a493b8b8796ea99a0
mao <1296193245@qq.com>
2024-01-04 15:33:06 +0800

    uniapp-device-accelerometer

---
+++ pages/index/test1/test1.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<h2>x:{{x}}</h2>
+		<h2>y:{{y}}</h2>
+		<h2>z:{{z}}</h2>
+		<button type="primary" @click="button1">监听加速度</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				x:0,
+				y:0,
+				z:0,
+			}
+		},
+		methods: {
+			button1()
+			{
+				uni.onAccelerometerChange(function(res)
+				{
+					this.x=res.x;
+					this.y=res.y;
+					this.z=res.z;
+					console.log(this.x,this.y,this.z);
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index/test2/test2.vue
@@ -0,0 +1,52 @@
+<template>
+	<view>
+		<h2>x:{{x}}</h2>
+		<h2>y:{{y}}</h2>
+		<h2>z:{{z}}</h2>
+		<button type="primary" @click="button1">监听加速度</button>
+		<button type="primary" @click="button2">取消监听加速度</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				x:0,
+				y:0,
+				z:0,
+				f:undefined,
+			}
+		},
+		methods: {
+			button1()
+			{
+				if(!this.f)
+				{
+					this.f =function(res)
+					{
+						this.x=res.x;
+						this.y=res.y;
+						this.z=res.z;
+						console.log(this.x,this.y,this.z);
+					}
+					uni.onAccelerometerChange(this.f)
+				}
+			},
+			button2()
+			{
+				if(this.f)
+				{
+					uni.offAccelerometerChange(this.f);
+					this.f=undefined;
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+button{
+	margin: 5px;
+}
+</style>
